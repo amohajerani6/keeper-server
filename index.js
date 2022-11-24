c8038b27112590183bec5a11ddaeafb27bfb9006
Amir Mohajerani <amohajerani6@gmail.com>
2022-11-24 16:37:22 -0500

    none

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-//app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+const cors = require("cors");
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 const mongoModelItems = require("./mongoModelItems");
 const mongoModelAccounts = require("./mongoModelAccounts");
 app.use(express.json());
